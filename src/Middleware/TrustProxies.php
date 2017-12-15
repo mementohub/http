c51afcff33b59d09b59114624f548005709e53ee
Andrei Penciu <andrei.penciu@mementohub.com>
2017-12-15 11:20:24 +0200

    add trust proxy middleware

---
+++ src/Middleware/TrustProxies.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace iMemento\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array
+     */
+    protected $proxies;
+
+    /**
+     * The current proxy header mappings.
+     *
+     * @var array
+     */
+    protected $headers = [
+        Request::HEADER_FORWARDED => 'FORWARDED',
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+    ];
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->proxies = [];
+        array_push($this->proxies, $request->server('REMOTE_ADDR'));
+
+        return parent::handle($request, $next);
+    }
+}
